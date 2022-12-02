f8f4d51f61a7ce91013471230833244b89720460
Maria Akifieva <mitafrumoasa@gmail.com>
2022-12-02 15:53:09 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,112 @@
+const CARDS_CONTENT = [
+  {
+    title: "Crush 1999",
+    text: `(By Peter Mason)\n
+The good lilac just beyond the house
+hangs in bloom like a lace dress.
+With no one home, I gather
+my young body from the closet,
+smell every floral and vased death,
+fit loose my sister’s robin’s egg
+skirt around my waist. Quiet,
+in her unlit room, I go tenderly
+to the mirror, my bare collarbone
+a trickled pond of redbirds
+that will ribbon unsoftly with time—
+but here, now, I am seven, and love
+so deeply a boy in second grade,
+I need to be pretty in the dark`,
+  },
+  {
+    title: "Fissured",
+    text: `(By Cindy Juyoung Ok) \n
+Several of my bones have splintered or
+separated and been left to heal incorrectly
+on their own, mostly in childhood. Of course
+the analyst believes this to be symbolic
+of my habitual denial of pain or, indeed,
+vulnerability, but she cannot admit this directly.
+She asks me leading questions like, am I truly
+feeling the feeling of suffering, which I
+find difficult to imagine. If I’m not feeling
+my pain, then who or what is the me in my?
+Instead, I announce that my partner comments
+often that I never complain and rarely experience
+side effects of a medication or symptoms
+of an illness. By providing evidence for her
+claim, I hope to lead her to accidentally provide
+some reasoning. I know she believes this hope
+for narrative arrangement, too, reflects repression
+of some deep ache. Still, I imagine physical pain
+as an intellectual relief, whereby thought stops
+being a primary expression. I read that Asian
+patients require less analgesic than the European
+ones and wonder about what I want, my Asian
+patience, against what I require. My nose broken
+twice created a sort of suffocating zigzag,
+permanent purple band along my bridge, so
+light it is imperceptible to almost everyone.
+An elbow to my face in front of a group made
+them suddenly concerned, so I smiled to render
+such disquiet unnecessary. I could not assume
+their concern was real, which is to say lasting,
+and I held, as I hold now, pain as ultimately
+shallow, never indicative of anything but itself.`,
+  },
+  {
+    title: "Do not go gentle into that good night",
+    text: `(Dylan Thomas)\n
+Do not go gentle into that good night,
+Old age should burn and rave at close of day;
+Rage, rage against the dying of the light.
+
+Though wise men at their end know dark is right,
+Because their words had forked no lightning they
+Do not go gentle into that good night.
+
+Good men, the last wave by, crying how bright
+Their frail deeds might have danced in a green bay,
+Rage, rage against the dying of the light.
+
+Wild men who caught and sang the sun in flight,
+And learn, too late, they grieved it on its way,
+Do not go gentle into that good night.
+
+Grave men, near death, who see with blinding sight
+Blind eyes could blaze like meteors and be gay,
+Rage, rage against the dying of the light.
+
+And you, my father, there on the sad height,
+Curse, bless, me now with your fierce tears, I pray.
+Do not go gentle into that good night.
+Rage, rage against the dying of the light.
+`,
+  },
+  {
+    title: "The Road Not Taken",
+    text: `(By Robert Frost)\n
+Two roads diverged in a yellow wood,
+And sorry I could not travel both
+And be one traveler, long I stood
+And looked down one as far as I could
+To where it bent in the undergrowth;
+
+Then took the other, as just as fair,
+And having perhaps the better claim,
+Because it was grassy and wanted wear;
+Though as for that the passing there
+Had worn them really about the same,
+
+And both that morning equally lay
+In leaves no step had trodden black.
+Oh, I kept the first for another day!
+Yet knowing how way leads on to way,
+I doubted if I should ever come back.
+
+I shall be telling this with a sigh
+Somewhere ages and ages hence:
+Two roads diverged in a wood, and I—
+I took the one less traveled by,
+And that has made all the difference.`,
+  },
+];
